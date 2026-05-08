986380d2fd4242b4414ae1afd53babacea3f9547
Michael Bishop <cleverca22@gmail.com>
2026-05-08 04:05:18 -0300

    update moar things

--- packages.nix
+++ packages.nix
@@ -4,6 +4,7 @@ let
   overlays = [
     iohk-nix.overlays.crypto
     haskellNix.overlay
+    iohk-nix.overlays.haskell-nix-crypto
   ];
   mkGitHook = revision: let
     hook = pkgs.writeShellScriptBin "git" ''
@@ -15,14 +16,26 @@ let
   pkgs = import nixpkgs { inherit overlays system; config.allowAliases = false; };
   ogmios-project = pkgs.haskell-nix.project {
     compiler-nix-name = "ghc967";
+    supportHpack = false;
     inputMap = {
       "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
       "https://chap.intersectmbo.org/" = CHaP;
     };
     projectFileName = "cabal.project";
-    src = ./server;
+    src =
+      # just /server subdir and filter package.yaml
+      # otherwise it fails on missing .hpack something yaml
+      pkgs.haskell-nix.haskellLib.cleanSourceWith {
+        name = "ogmios-src";
+        src = ./.;
+        subDir = "server";
+        filter = path: type:
+          builtins.all (x: x) [
+            (baseNameOf path != "package.yaml")
+          ];
+      };
     modules = [
-      (mkGitHook self.rev)
+      #(mkGitHook self.rev)
       ({ lib, pkgs, ... }:
       {
         packages.cardano-crypto-class.components.library.pkgconfig = lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 pkgs.libblst ] ];
@@ -30,6 +43,6 @@ let
     ];
   };
 in {
-  inherit ogmios-project;
+  default = ogmios-project.ogmios.components.exes.ogmios;
   inherit (pkgs) libblst;
 }
